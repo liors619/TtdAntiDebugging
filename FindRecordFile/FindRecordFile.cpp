81e48cd9e83742e95a83cc7778ec58a6072c95df
Lior Swisa <lior.swisa@campus.technion.ac.il>
2022-01-11 19:39:30 +0200

    find record file by the real name of the process running

--- FindRecordFile/FindRecordFile.cpp
+++ FindRecordFile/FindRecordFile.cpp
@@ -3,16 +3,28 @@
 
 #include <iostream>
 #include <filesystem>
+#include <windows.h>
+#include <Psapi.h>
+#include <string>
 
 using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 using namespace std;
 
+string GetCurrentExecutableNameWithoutExtension() {
+	char processExeName[MAX_PATH];
+	GetModuleBaseNameA(GetCurrentProcess(), 0, processExeName, MAX_PATH);
+	string processExeNameAsString(processExeName);
+	return processExeNameAsString.substr(0, processExeNameAsString.find_last_of('.'));
+}
+
 int main()
 {
-	for (const auto& dirEntry : recursive_directory_iterator("D:\\", std::filesystem::directory_options::skip_permission_denied)) {
+	string exeNameWithoutExtension = GetCurrentExecutableNameWithoutExtension();
+
+	for (const auto& dirEntry : recursive_directory_iterator("D:\\", filesystem::directory_options::skip_permission_denied)) {
 		try {
 			// we can check what our process name so researcher could not change it
-			if (dirEntry.path().filename().string()._Starts_with("RecordedProgramName") && 
+			if (dirEntry.path().filename().string()._Starts_with(exeNameWithoutExtension) &&
 				dirEntry.path().filename().extension().string() == ".run") {
 				cout << dirEntry.path() << endl;
 				system("pause");
